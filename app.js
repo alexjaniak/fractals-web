39787b86bce7fc8b9e7de6aaa4c77670f8a0e6a4
Alex Janiak (Mac) <alexander.janiak.acc@outlook.com>
2021-02-15 01:14:23 -0500

    created main files

---
+++ app.js
@@ -0,0 +1,7 @@
+var Init = function() {
+    var canvas = document.getElementById("glCanvas");
+    var gl = canvas.getContext("webgl");
+
+    gl.clearColor(0,0,0,1.0)
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
